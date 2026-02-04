5aa2d300f16eb3edcc3081919e17977143c47ea6
Mark <mrkswrn@gmail.com>
2026-02-04 11:43:46 +0100

    remove compression middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -25,7 +25,7 @@ async function proxyRequest(request: Request, targetBaseUrl: string) {
   });
 }
 
-export const auth = defineMiddleware(async (context, next) => {
+export const onRequest = defineMiddleware(async (context, next) => {
   const pathname = new URL(context.request.url).pathname;
 
   // Handle auth (api/auth) and api on fastapi side
@@ -51,38 +51,3 @@ export const auth = defineMiddleware(async (context, next) => {
 
   return context.redirect("/auth/login");
 });
-
-/**
- * Compress responses using gzip or deflate
- * TODO: astro static files are not compressed as they
- * don't go through the middleware
- */
-const compress = defineMiddleware(async (context, next) => {
-  const response = await next();
-  const accept = context.request.headers.get("accept-encoding") || "";
-  const alreadyEncoded = response.headers.get("content-encoding");
-
-  let body: BodyInit | null | undefined = response.body;
-  let encoding: string | null = null;
-  if (!alreadyEncoded && accept.includes("gzip")) {
-    body = response.body?.pipeThrough(new CompressionStream("gzip"));
-    encoding = "gzip";
-  } else if (!alreadyEncoded && accept.includes("deflate")) {
-    body = response.body?.pipeThrough(new CompressionStream("deflate"));
-    encoding = "deflate";
-  }
-
-  if (encoding) {
-    return new Response(body, {
-      ...response,
-      headers: {
-        ...response.headers,
-        "Content-Encoding": encoding,
-      },
-    });
-  }
-
-  return response;
-});
-
-export const onRequest = sequence(auth, compress);
